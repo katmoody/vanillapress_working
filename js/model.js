e666bba03b689414cf083570fd2a628e76dd76c7
Katrina Moody <katmoody@users.noreply.github.com>
2018-01-10 11:56:44 -0500

    Update model.js

--- js/model.js
+++ js/model.js
@@ -9,16 +9,18 @@
 
 var model = {};
 
+model.init = function() {
+ 
+ model.updateLocalStore( jsonData );
+ 
+};
+
 /**
   * Gets posts from local store
   *
   * @return posts {array} An array of post objects
   */
-model.getLoganStore = function() {
- 
- return JSON.parse( localStorage.getItem( 'vanillaPress' ) );
- 
-};
+
 
 /**
   * Gets content from local store
@@ -26,15 +28,21 @@ model.getLoganStore = function() {
   * @return store {object} Object or array of objects of site data
   */
 
+model.getLocalStore = function() {
+ 
+ return JSON.parse( localStorage.getItem( 'vanillaPress' ) );
+ 
+};
 
 /**
   * Saves temporary store to local storage.
   *
   * @param store {string} JSON string of data to store
   */
-model.getLocalStore = function() {
 
-  return JSON.parse( localStorage.getItem( 'vanillaPress' ) );
+model.updateLocalStore = function( store ) {
+
+  localStorage.setItem( 'vanillaPress', store);
 
 };
 
