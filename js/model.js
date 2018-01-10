b195e755b304ad6d72df49c9d35559ca959f38bd
Katrina Moody <katmoody@users.noreply.github.com>
2018-01-10 11:11:14 -0500

    Update model.js

--- js/model.js
+++ js/model.js
@@ -1,1 +1,38 @@
+/**
+ * Model file for working with data
+ */
+
+/**
+ * Main Model Object
+ *
+ */
+
+var model = {};
+
+/**
+  * Gets posts from local store
+  *
+  * @return posts {array} An array of post objects
+  */
+
+
+/**
+  * Gets content from local store
+  *
+  * @return store {object} Object or array of objects of site data
+  */
+
+
+/**
+  * Saves temporary store to local storage.
+  *
+  * @param store {string} JSON string of data to store
+  */
+
+
+
+/**
+  * Deletes data from local storage
+  *
+  */
 
