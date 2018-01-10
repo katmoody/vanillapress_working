89750387d85b9298b3fb4ddad4c5becf1feea5b7
Katrina Moody <katmoody@users.noreply.github.com>
2018-01-10 12:19:46 -0500

    Update view.js

--- js/view.js
+++ js/view.js
@@ -2,3 +2,36 @@
  * View file for displaying content
  */
 
+/**
+* Main view object
+*/
+
+var view = {};
+
+/**
+* Calls initial View methods
+*/
+
+view.init = function() {
+  
+  
+  
+};
+
+/**
+* Get blog posts and append them to the page
+*/
+
+view.loadBlogPosts = function() {
+  
+  var posts = model.getPosts(),
+      postsMarkup = document.createDocumentFragment(),
+      primaryContentEl = helpers.getPageContentEl();
+  
+};
+
+/**
+* Create Markup for Blog Posts
+* @param object {post} Post to create markup for 
+* @return object {articleEl} Final post markup
+*/
